85194e16cbfb39864ef2eff42a3c8308624480cb
Arnav Sawant <arnavsawant.as@gmail.com>
2024-08-16 20:38:54 +0530

    additional changes to npm + controllers+ mongoDB

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,10 @@
 // auth.route.js is named that was to show that it is a route file
 
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controllers.js"; // the .js is neeed as we have the type as module in the package cuz we are importing files from local machine 
 
 const router = express.Router();
-app.get("/signup" , (req,res)=>{
-    res.send("Signup route");
-});
-app.get("/login" , (req,res)=>{
-    res.send("login route");
-});
-app.get("/logout" , (req,res)=>{
-    res.send("logout route");
-});
+app.get("/signup" , signup);
+app.get("/login" , login);
+app.get("/logout" , logout);
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 // const express = require('express); // commonJS
 import express from "express";  // ems emmascript
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 
+
+dotenv.config();
+console.log("MONGO URI :", process.env.MONGO_URI )
 const app = express();
 
-app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/auth",authRoutes); // the other ones like the signup logi nand logout croutesw will be under the auth path
 
 app.listen (5000,()=>{
     console.log('Server started at http://localhost:5000');
