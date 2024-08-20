65095424a3271a94e21efc2fc66d4195144a8465
Arnav Sawant <arnavsawant.as@gmail.com>
2024-08-20 22:45:49 +0530

    assets and backend controllers

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -7,9 +7,42 @@ export async function signup (req,res){
    } 
 
    const emailRegex =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
-   
+
+   if (!emailRegex.test(email)){
+    return res.status(400).json({success:false,message:"Invalid Email !"})
+   }
+   if(password.length < 6){
+    return res.status(400).json({success:false,message: " Password must be atleast 6 characters "})
+   }
+
+   const existingUserByEmail = await User.findOne({email:email})
+
+   if (existingUserByEmail){
+    return res.status(400).json({success:false,message:"Email already exists"})
+   }
+ 
+const existingUserByUsername = await User.findOne({username : username})
+
+if (existingUserByUsername){
+    return res.status(400).json({success:false,message:"Username already exists"})  
+}
+
+const PROFILE_PICS = ["/avatar1.png", "/avatar2.png" , "avatar3.png"];
+const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)]; //random pf pic
+
+ const newUser = new User({
+     email:email,
+     password:password,
+     username:password,
+     image:image,
+ })
+ //creatred user should be saved in database
+
+ await newUser.save();
+
 }catch (error) {
-    
+    console.log("Error on signup controller",error.message);
+    res.status(500).json({success:false,message:"Internal server error"});
 }
 }
 
